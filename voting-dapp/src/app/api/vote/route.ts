76f729ebe7889db03e474c1331a1af334d42b799
Shekhar S <shekharshashank97@gmail.com>
2025-03-20 15:58:25 +0530

    Fix:
    Updated code in post methods and validated transation

--- voting-dapp/src/app/api/vote/route.ts
+++ voting-dapp/src/app/api/vote/route.ts
@@ -1,5 +1,9 @@
-import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest } from "@solana/actions"
-import { Connection, PublicKey } from "@solana/web3.js";
+import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest, createPostResponse } from "@solana/actions"
+import { Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { Voting } from '@/../anchor/target/types/voting';
+import { BN, Program } from "@coral-xyz/anchor";
+const IDL = require('@/../anchor/target/idl/voting.json');
+
 
 export const OPTIONS = GET;
 export async function GET(request: Request) {
@@ -33,13 +37,35 @@ export async function POST(request: Request) {
     }
 
     const connection = new Connection('http://127.0.0.1:8899', 'confirmed');
+    const program: Program<Voting> = new Program(IDL, { connection });
 
     const body: ActionPostRequest = await request.json();
     let voter;
     try {
-
         voter = new PublicKey(body.account);
     } catch (err) {
         return new Response("Invalid candidate", { status: 400, headers: ACTIONS_CORS_HEADERS });
     }
+
+    const instruction = await program.methods
+        .vote(candidate, new BN(1))
+        .accounts({
+            signer: voter,
+        })
+        .instruction();
+
+    const blockhash = await connection.getLatestBlockhash();
+    const transaction = new Transaction({
+        feePayer: voter,
+        blockhash: blockhash.blockhash,
+        lastValidBlockHeight: blockhash.lastValidBlockHeight,
+    }).add(instruction);
+
+    const response = await createPostResponse({
+        fields: {
+            transaction: transaction,
+        }
+    });
+
+    return Response.json(response, { headers: ACTIONS_CORS_HEADERS });
 }
